45b06c6498c86d03da50f33bee1fc052d6be1bdd
Adrielso Kunert <adrielsokunert@gmail.com>
2026-04-13 11:04:36 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+fetch("productos.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const contenedor = document.getElementById("productos");
+
+    data.forEach((prod) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <div class="card-top" style="background:${prod.color}">
+          <span class="tag">${prod.etiqueta}</span>
+          <img src="${prod.imagen}" alt="">
+        </div>
+        <div class="card-body">
+          <h4>${prod.nombre}</h4>
+          <p>${prod.descripcion}</p>
+          <div class="price">R$ ${prod.precio.toFixed(2)}</div>
+          <button>VER MAIS</button>
+        </div>
+      `;
+
+      contenedor.appendChild(card);
+    });
+  });
