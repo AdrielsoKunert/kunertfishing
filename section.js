0ab95961c9d0b7e958f80208b74882108104f6c3
Adrielso Kunert <adrielsokunert@gmail.com>
2026-04-14 14:20:50 -0300

    Update section.js

--- section.js
+++ section.js
@@ -1,6 +1,13 @@
 function showSection(id, el) {
-  document.querySelectorAll('section').forEach(s => s.style.display = 'none');
+  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
+  document.querySelectorAll('.nav-menu a').forEach(a => a.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  el.classList.add('active');
+}
+/*function showSection(id, el) {
+  document.querySelectorAll('.section').forEach(s => s.style.display = 'none');
   document.querySelectorAll('.nav-menu a').forEach(a => a.classList.remove('active'));
   document.getElementById('section-' + id).style.display = 'block';
   el.classList.add('active');
 }
+*/
