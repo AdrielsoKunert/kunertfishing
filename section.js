194f993cbd79a62817a54690340d95eec2fb3846
Adrielso Kunert <adrielsokunert@gmail.com>
2026-04-14 13:22:15 -0300

    Create section.js

---
+++ section.js
@@ -0,0 +1,6 @@
+function showSection(id, el) {
+  document.querySelectorAll('section.products').forEach(s => s.style.display = 'none');
+  document.querySelectorAll('.nav-menu a').forEach(a => a.classList.remove('active'));
+  document.getElementById('section-' + id).style.display = 'block';
+  el.classList.add('active');
+}
