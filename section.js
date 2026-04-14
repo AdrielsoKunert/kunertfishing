2c9ddd5c7a6a913e9d06370af0ef6fa8dcacad43
Adrielso Kunert <adrielsokunert@gmail.com>
2026-04-14 13:52:57 -0300

    Update section.js

--- section.js
+++ section.js
@@ -1,5 +1,5 @@
 function showSection(id, el) {
-  document.querySelectorAll('section-iscas').forEach(s => s.style.display = 'none');
+ /* document.querySelectorAll('section-iscas').forEach(s => s.style.display = 'none');*/
   document.querySelectorAll('.nav-menu a').forEach(a => a.classList.remove('active'));
   document.getElementById('section-' + id).style.display = 'block';
   el.classList.add('active');
